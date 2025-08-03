5ce90cf482ef525c13a2ef22110cafa3c5bbc80b
aulaenlanube <edtorlla@gmail.com>
2025-08-03 09:16:55 +0200

    cambios ligeros UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,18 @@ import { Outlet, useLocation } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 
 function App() {
-  // --- CÓDIGO AÑADIDO ---
-  // Obtiene la ubicación actual de la página
   const { pathname } = useLocation();
 
-  // Este efecto se ejecuta cada vez que la ruta (pathname) cambia
   useEffect(() => {
-    // Sube el scroll de la ventana a la posición superior (0, 0)
     window.scrollTo(0, 0);
-  }, [pathname]); // El efecto se activa solo cuando el pathname es diferente
+  }, [pathname]);
 
   return (
     <>
-      <Helmet>
-        <title>EduApps - Apps Educativas para Primaria y ESO</title>
-        <meta name="description" content="Descubre las mejores apps educativas organizadas por cursos." />
+      <Helmet>        
+        <title>Apps Educativas para Primaria y secundaria en España</title>
+        <meta name="description" content="Descubre las mejores apps educativas gratuitas, organizadas por cursos de Primaria y ESO para aprender jugando." />
+        <meta name="author" content="Edu Torregrosa" />
       </Helmet>
       
       <Outlet />
